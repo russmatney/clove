2537a521e73d6116459dd5aab260b54d334f6251
Russell Matney <russell.matney@gmail.com>
2025-01-03 12:48:36 -0500

    deps: update tauri to 2.0
    There might be a simpler way to create a webview now, but this seems to
    work the same, at least.
    
    There's still smearing/issues with transparency and updates, not sure
    what that's about yet.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,13 +4,22 @@
 )]
 
 use url::Url;
-use std::path::PathBuf;
-use tauri::Icon;
 
 fn main() {
+    use tauri_plugin_cli::CliExt;
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_http::init())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_cli::init())
         .setup(|app| {
-            match app.get_cli_matches() {
+            match app.cli().matches() {
                 Ok(matches) => match matches.subcommand {
                     Some(subcom) => match subcom.name.as_str() {
                         "create-window" => {
@@ -19,7 +28,6 @@ fn main() {
                             let title = &subcom.matches.args.get("title").clone().unwrap().value;
                             let label = &subcom.matches.args.get("label").clone().unwrap().value;
                             let url = &subcom.matches.args.get("url").clone().unwrap().value;
-                            let icon_path = &subcom.matches.args.get("icon").clone().unwrap().value;
                             let focused = &subcom
                                 .matches
                                 .args
@@ -48,17 +56,16 @@ fn main() {
                             println!("title {:?}", title);
                             println!("label {:?}", label);
                             println!("url {:?}", url);
-                            println!("icon_path {:?}", icon_path);
                             println!("focused {:?}", focused);
                             println!("transparent {:?}", transparent);
                             println!("decorations {:?}", decorations);
 
                             let proper_url = Url::parse(&url.as_str().unwrap()).unwrap();
 
-                            let mut win = tauri::WindowBuilder::new(
+                            let win = tauri::WebviewWindowBuilder::new(
                                 app,
                                 label.as_str().unwrap(),
-                                tauri::WindowUrl::External(proper_url),
+                                tauri::WebviewUrl::External(proper_url),
                             )
                             .title(title.as_str().unwrap())
                             .resizable(true)
@@ -70,16 +77,7 @@ fn main() {
                             .focused(focused.unwrap())
                             .inner_size(800.0, 800.0);
 
-                            win = match icon_path.as_str() {
-                                Some(path) => {
-                                    let i = Icon::File(PathBuf::from(path));
-                                    win.icon(i)?
-                                }
-                                _ => win
-                            };
-
                             win.build()?;
-
                         }
                         _ => println!(
                             "No matching subcommand found. (Did you mean 'create-window'?)"
